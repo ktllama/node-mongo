10ce6b9e22c0809d887f7b05c82989835c38fd0e
Katie <costantinkp@gmail.com>
2022-03-01 11:55:09 -0500

    changed from callbacks to promises

--- index.js
+++ index.js
@@ -9,55 +9,109 @@ const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 //database name we want to connect to
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-//allows us to connect mongo client with mdb server
-//first argument url, second object that sets various options
-//third arg is callback w. err and client param
-//use client to connect to database and do various operations
-    assert.strictEqual(err, null);
-//check to make sure err is not null
-//first value is actual argument we are checking, second is to see the argument we are checking against to see if the first === the second- if they are === we continue, if not the asser will fail
-//when an assert fails it throws an error, terminates entire app and logs to console what went wrong- if not it will continue on will application
+MongoClient.connect(url, { useUnifiedTopology: true })
+    .then(client => {
+
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-//passing dbname we set above to this method- it will connect us to the nucampsite database on the mongo db server
-//we can use this db object to asccess a set of methods to interact with that database
-    db.dropCollection('campsites', (err, result) => {
-        //emptying db so when we test it we start w blank one each time- not normal practice
-        //drop means delete
-        assert.strictEqual(err, null);
-        console.log('Dropped Collection', result);
-        
-        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
-        'campsites', result => {
+
+    db.dropCollection('campsites')
+    .then(result => {
+        console.log('Dropped Collection:', result);
+    })
+    .catch(err => console.log('No collection to drop.'));
+
+    //continue to next method
+    dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites')
+    .then(result => {
         console.log('Insert Document:', result.ops);
 
-        dboper.findDocuments(db, 'campsites', docs => {
-            console.log('Found Documents:', docs);
+        return dboper.findDocuments(db, 'campsites');
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
 
-            dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
-                { description: "Updated Test Description" }, 'campsites',
-                result => {
-                    console.log('Updated Document Count:', result.result.nModified);
+        return dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+            { description: "Updated Test Description" }, 'campsites');
+    })
+    .then(result => {
+        console.log('Updated Document Count:', result.result.nModified);
 
-                    dboper.findDocuments(db, 'campsites', docs => {
-                        console.log('Found Documents:', docs);
-                        
-                        dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
-                            'campsites', result => {
-                                console.log('Deleted Document Count:', result.deletedCount);
+        return dboper.findDocuments(db, 'campsites');
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
 
-                                client.close();
-                            }
-                        );
-                    });
-                }
-            );
-        });
+        return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+            'campsites');
+    })
+    .then(result => {
+        console.log('Deleted Document Count:', result.deletedCount);
+
+        return client.close();
+    })
+    .catch(err => {
+        console.log(err);
+        client.close();
     });
-});
-});
+})
+.catch(err => console.log(err));
+
+
+
+
+//OLD WAY WE WROTE IT USING CALLBACKS
+
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+// //allows us to connect mongo client with mdb server
+// //first argument url, second object that sets various options
+// //third arg is callback w. err and client param
+// //use client to connect to database and do various operations
+//     assert.strictEqual(err, null);
+// //check to make sure err is not null
+// //first value is actual argument we are checking, second is to see the argument we are checking against to see if the first === the second- if they are === we continue, if not the asser will fail
+// //when an assert fails it throws an error, terminates entire app and logs to console what went wrong- if not it will continue on will application
+//     console.log('Connected correctly to server');
+
+//     const db = client.db(dbname);
+// //passing dbname we set above to this method- it will connect us to the nucampsite database on the mongo db server
+// //we can use this db object to asccess a set of methods to interact with that database
+//     db.dropCollection('campsites', (err, result) => {
+//         //emptying db so when we test it we start w blank one each time- not normal practice
+//         //drop means delete
+//         assert.strictEqual(err, null);
+//         console.log('Dropped Collection', result);
+
+//         dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
+//         'campsites', result => {
+//         console.log('Insert Document:', result.ops);
+
+//         dboper.findDocuments(db, 'campsites', docs => {
+//             console.log('Found Documents:', docs);
+
+//             dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+//                 { description: "Updated Test Description" }, 'campsites',
+//                 result => {
+//                     console.log('Updated Document Count:', result.result.nModified);
+
+//                     dboper.findDocuments(db, 'campsites', docs => {
+//                         console.log('Found Documents:', docs);
+                        
+//                         dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+//                             'campsites', result => {
+//                                 console.log('Deleted Document Count:', result.deletedCount);
+
+//                                 client.close();
+//                             }
+//                         );
+//                     });
+//                 }
+//             );
+//         });
+//     });
+// });
+// });
 
 //in this code brought mongo client in and connected to it to connect this client with server.  It gave us an object to access campsite database. we deleted then re created this database and added a document.  we then used find to find all documents in this database.  we then closed out with the client and finished
 //this code was a series of callback messages- this kind of callback nesting is not great
--- operations.js
+++ operations.js
@@ -2,35 +2,24 @@ const assert = require('assert').strict;
 
 //four methods to export
 //like CRUD operations
+//if we dont give callback it automatically returns results in promise
 
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
